1b95893fbed851712315423340d11d73f5213b9a
waalsh <annawalsh221@gmail.com>
2023-12-04 16:46:17 -0500

    day 4 problem 2 solve

--- day4/a.py
+++ day4/a.py
@@ -8,7 +8,8 @@ class Card:
         self.winning = winning
         self.drawn = drawn
         self.winCount = self.countWinningNumbers()
-        self.points = self.getPoints()
+        self.copies = 1
+        
 
     def printNice(self):
         # print("for card", self.num, ":")
@@ -24,7 +25,7 @@ class Card:
         self.winCount = winCount
         return winCount
     
-    def getPoints(self):
+    def getPoints(self): # just for problem 1
         if self.winCount > 0:
             return pow(2, self.winCount - 1)
         else:
@@ -32,27 +33,47 @@ class Card:
 
 
 # filename = "test_input.txt"
+# table_length = 6
 filename = "input.txt"
+table_length = 211
 with open(filename, 'r') as file:
     # Parsing the file into Games
-    cards = []
-    sum_of_power = 0
+    cards = {}
+    i = 1
     for line in file:        
         line = line.strip()
         card_and_nums = line.split(":")
         digit = int(re.search(r'\d+', card_and_nums[0]).group())
+        if not digit == i:
+            print("uh oh, something weird about line ", i)
         nums = card_and_nums[1].split("|")
 
         winning = [x.group() for x in re.finditer(r'\d+', nums[0])]
         drawn = [x.group() for x in re.finditer(r'\d+', nums[1])]
         
         c = Card(digit, winning, drawn)
-        cards.append(c)
+        cards[str(digit)] = c
+        i+=1
 
-    sum = 0
-    for c in cards:
-        c.printNice()
-        sum += c.points
-    print(sum)
+    print(cards)
+
+    scratchCard_count = 0
+    debug = True
+    for i in range (1, table_length + 1):
+        debug = i % 10 == 0;
+        card = cards[str(i)]
+        scratchCard_count += card.copies
+        if debug:
+            print("for card", i, "with", card.copies, "copies and", card.winCount, "wins")
+        for j in range(card.copies):
+            for k in range(i + 1, min(i + card.winCount + 1, table_length + 1)):
+                # increment the number of copies
+                if debug:
+                    print("making a copy of card", k)
+                cards[str(k)].copies += 1
+
+    print(scratchCard_count)
+
+
     
 
